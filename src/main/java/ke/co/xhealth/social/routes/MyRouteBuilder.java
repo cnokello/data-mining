cecc292eb432dfb85a729ab5304d920a39f533e9
Okello Nelson <cn.okello@live.com>
2014-03-31 22:27:41 +0300

    Spring config

--- src/main/java/ke/co/xhealth/social/routes/MyRouteBuilder.java
+++ src/main/java/ke/co/xhealth/social/routes/MyRouteBuilder.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.camel.example.spring;
+package ke.co.xhealth.social.routes;
 
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.spring.Main;
