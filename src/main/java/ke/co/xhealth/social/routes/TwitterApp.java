1107aee011314e4d78a132359884953bd666294c
Okello Nelson <cn.okello@live.com>
2014-03-31 22:55:48 +0300

    Twitter configs

--- src/main/java/ke/co/xhealth/social/routes/TwitterApp.java
+++ src/main/java/ke/co/xhealth/social/routes/TwitterApp.java
@@ -1,56 +1,30 @@
-/**
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package ke.co.xhealth.social.routes;
 
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.spring.Main;
+import org.springframework.beans.factory.annotation.Value;
 
 /**
- * A simple example router from a file system to an ActiveMQ queue and then to a file system
- *
- * @version 
+ * This processing route posts new entries into twitter
+ * 
+ * @author nelson.okello
+ * 
  */
-public class MyRouteBuilder extends RouteBuilder {
+public class TwitterApp extends RouteBuilder {
 
-    /**
-     * Allow this route to be run as an application
-     */
-    public static void main(String[] args) throws Exception {
-        new Main().run(args);
-    }
+  private @Value("${api.key}")
+  String API_KEY;
 
-    public void configure() {
-        // populate the message queue with some messages
-        from("file:src/data?noop=true").
-                to("jms:test.MyQueue");
+  private @Value("${api.secret}")
+  String API_SECRET;
 
-        from("jms:test.MyQueue").
-                to("file://target/test?noop=true");
+  private @Value("${access.token}")
+  String ACCESS_TOKEN;
 
-        // set up a listener on the file component
-        from("file://target/test?noop=true").
-                bean(new SomeBean());
-    }
+  private @Value("${access.secret}")
+  String ACCESS_TOKEN_SECRET;
 
-    public static class SomeBean {
-
-        public void someMethod(String body) {
-            System.out.println("Received: " + body);
-        }
-    }
+  public void configure() {
+    from("timer://testTimer?period=5000").log("API KEY: " + API_KEY + "\n");
+  }
 
 }
